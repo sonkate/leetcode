1a3e86aeccb5fe0d08ffdb51b24c9c7067af91b3
Son Tran <son.tt@trans-tech.vn>
2026-06-30 14:22:48 +0700

    [FEAT][https://leetcode.com/problems/backspace-string-compare] : backspace str compare

---
+++ 28.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var backspaceCompare = function (s, t) {
+    let stackS = [];
+    let stackT = [];
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === '#') {
+            stackS.pop();
+        } else {
+            stackS.push(s[i]);
+        }
+    }
+    for (let i = 0; i < t.length; i++) {
+        if (t[i] === '#') {
+            stackT.pop();
+        } else {
+            stackT.push(t[i]);
+        }
+    }
+    return stackS.join('') === stackT.join('');
+};
